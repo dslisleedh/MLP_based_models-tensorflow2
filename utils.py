3e31b3a32307bd4d6ccfddb3b77d4782bfb9c0c7
dslisleedh <gnova6kud@gmail.com>
2022-05-20 09:14:02 +0900

    fixed StochasticDepth

--- utils.py
+++ utils.py
@@ -8,6 +8,11 @@ class Droppath(tf.keras.layers.Layer):
 
     @tf.function
     def call(self, inputs, **kwargs):
+        if self.prob == 1.:
+            return inputs
+        if self.prob == 0.:
+            return tf.zeros_like(inputs)
+
         if tf.keras.backend.learning_phase():
             epsilon = tf.keras.backend.random_bernoulli(shape=[tf.shape(inputs)[0]] + [1 for _ in range(len(tf.shape(inputs)) - 1)],
                                                         p=self.prob,
